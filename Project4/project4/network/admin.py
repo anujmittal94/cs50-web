be4e565504932dd77a1cf807f11b3ef67275ac90
anujmittal94 <anujanilmittal@gmail.com>
2021-05-17 13:50:11 +0400

    Models created

--- Project4/project4/network/admin.py
+++ Project4/project4/network/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from .models import User, Post, Follow
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("id", "username")
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("id", "poster", "post")
+
+class FollowAdmin(admin.ModelAdmin):
+    list_display = ("id", "follower", "following")
+
 # Register your models here.
+admin.site.register(User, UserAdmin)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Follow, FollowAdmin)
--- Project4/project4/network/models.py
+++ Project4/project4/network/models.py
@@ -4,3 +4,14 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    poster = models.ForeignKey(User, on_delete=models.CASCADE, related_name="postsbyuser")
+    post = models.TextField(max_length = 500)
+    timestamp = models.DateTimeField(auto_now_add=True, blank = True)
+    likes = models.ManyToManyField(User, related_name="likesonpost")
+
+
+class Follow(models.Model):
+    follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name="followsbyuser")
+    following = models.ForeignKey(User, on_delete=models.CASCADE, related_name="followsonuser")
